52e2c0ce59a06070d71a468d3821ad4cdbcfcc17
nitishsingh41 <45527813+nitishsingh41@users.noreply.github.com>
2023-10-25 11:35:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import plotly.graph_objects as go
+import plotly.io as pio
+
+# Create a sample Plotly figure
+fig = go.Figure(data=go.Scatter(x=[1, 2, 3, 4], y=[10, 11, 12, 13]))
+
+fig.show()
+output_filename = 'output.png'
+
+# Use the save_as function to save the figure as an image
+pio.write_image(fig, output_filename)
+st.write('output saved')
+with open("output.png", "rb") as file:
+    btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name="output.png",
+            mime="image/png"
+          )
